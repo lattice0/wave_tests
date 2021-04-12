bd40b15e29fed15375d87afd5404f0cce53f93f6
Lucas Zanella <me@lucaszanella.com>
2021-04-12 19:16:48 -0300

    works

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
-#include <Speaker.h>
+#include <iostream>
+#include <system_error>
 
-using namespace std;
-using namespace Hello;
+#include "wave/file.h"
 
-int main(int argc, char *argv[]) {
-  Speaker* speaker = new Speaker();
+int main() {
+  // read file's content
+  wave::File read_file;
+  wave::Error err = read_file.Open("sound.wav", wave::kIn);
+  if (err) {
+    std::cout << "Something went wrong in in open" << std::endl;
+    return 1;
+  }
 
-  speaker->sayHello();  
+  std::cout << "sample_rate: " << read_file.sample_rate() << std::endl;
+  std::cout << "bits_per_sample: " << read_file.bits_per_sample() << std::endl;
+  std::cout << "channel_number: " << read_file.channel_number() << std::endl;
+
+  std::vector<float> content;
+  err = read_file.Read(&content);
+  if (err) {
+    std::cout << "Something went wrong in read" << std::endl;
+    return 2;
+  }
+
+  return 0;
 }
